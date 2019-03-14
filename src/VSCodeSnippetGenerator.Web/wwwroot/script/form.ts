c2d4bc7f6f2540ee79333e47b435c13880288292
AdrianWilczynski <adrian.k.wilczynski@gmail.com>
2019-03-15 00:47:38 +0100

    Add similar behavior to ConvertToTabs checkbox

--- src/VSCodeSnippetGenerator.Web/wwwroot/script/form.ts
+++ src/VSCodeSnippetGenerator.Web/wwwroot/script/form.ts
@@ -3,3 +3,9 @@ descriptionArea.addEventListener('input', event => {
     let hasDescriptionCheckbox = document.getElementById('HasDescription') as HTMLInputElement
     hasDescriptionCheckbox.checked = descriptionArea.value.length > 0;
 });
+
+let tabLengthInput = document.getElementById('TabLength') as HTMLInputElement;
+tabLengthInput.addEventListener('input', evnet => {
+    let convertToTabsCheckbox = document.getElementById('ConvertToTabs') as HTMLInputElement;
+    convertToTabsCheckbox.checked = !!tabLengthInput.value;
+})
