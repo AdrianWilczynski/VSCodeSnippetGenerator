9a813ca406db70403957d76da63a7952697eadc6
AdrianWilczynski <adrian.k.wilczynski@gmail.com>
2019-03-15 14:17:50 +0100

    Replace string literals with constant

--- src/VSCodeSnippetGenerator.Web/Constants/Indentation.cs
+++ src/VSCodeSnippetGenerator.Web/Constants/Indentation.cs
@@ -7,14 +7,20 @@ namespace VSCodeSnippetGenerator.Web.Constants
     {
         public static IEnumerable<(string language, string tool, int tabLength)> PerLanguage => new List<(string language, string, int)>
         {
-            ( "C#", "Visual Studio 2019", 4 ),
-            ( "CSS", "Visual Studio 2019", 4),
-            ( "HTML", "Visual Studio 2019", 4),
-            ( "JS/TS", "Prettier", 2 ),
-            ( "JS/TS", "Visual Studio 2019", 4 ),
-            ( "JSON", "Visual Studio 2019", 2 ),
-            ( "SQL", "Visual Studio 2019", 4 ),
-            ( "XML", "Visual Studio 2019", 2 )
+            ( "C#", Tools.VisualStudio2019, 4 ),
+            ( "CSS",  Tools.VisualStudio2019, 4),
+            ( "HTML",  Tools.VisualStudio2019, 4),
+            ( "JS/TS", Tools.Prettier, 2 ),
+            ( "JS/TS",  Tools.VisualStudio2019, 4 ),
+            ( "JSON",  Tools.VisualStudio2019, 2 ),
+            ( "SQL",  Tools.VisualStudio2019, 4 ),
+            ( "XML",  Tools.VisualStudio2019, 2 )
         }.OrderBy(i => i.language);
+
+        private static class Tools
+        {
+            public const string VisualStudio2019 = "Visual Studio 2019";
+            public const string Prettier = "Prettier";
+        }
     }
 }
