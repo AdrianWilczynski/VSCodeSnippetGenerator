85b45269a6172db93e4f5064dfe047b84590342b
AdrianWilczynski <adrian.k.wilczynski@gmail.com>
2019-03-15 13:55:03 +0100

    Add datalist to help with choosing proper tab length

---
+++ src/VSCodeSnippetGenerator.Web/Constants/Indentation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace VSCodeSnippetGenerator.Web.Constants
+{
+    public static class Indentation
+    {
+        public static IEnumerable<(string language, int tabLength)> PerLanguage => new List<(string language, int tabLength)>
+        {
+            ( "C#", 4 ),
+            ( "CSS", 2),
+            ( "HTML", 4),
+            ( "JS/TS (Prettier)", 2 ),
+            ( "JS/TS (VSCode/Visual Studio 2019)", 4 ),
+            ( "JSON", 2 ),
+            ( "SQL", 4 ),
+            ( "XML", 2 )
+        }.OrderBy(i => i.language);
+    }
+}
