972387ebd3d800ef4626118774aced01c8bc9c36
AdrianWilczynski <adrian.k.wilczynski@gmail.com>
2019-03-15 14:28:04 +0100

    Get rid of year part from Visual Studio constant - it's kinda messing up list filtering

--- src/VSCodeSnippetGenerator.Web/Constants/Indentation.cs
+++ src/VSCodeSnippetGenerator.Web/Constants/Indentation.cs
@@ -7,19 +7,19 @@ namespace VSCodeSnippetGenerator.Web.Constants
     {
         public static IEnumerable<(string language, string tool, int tabLength)> PerLanguage => new List<(string language, string, int)>
         {
-            ( "C#", Tools.VisualStudio2019, 4 ),
-            ( "CSS",  Tools.VisualStudio2019, 4),
-            ( "HTML",  Tools.VisualStudio2019, 4),
+            ( "C#", Tools.VisualStudio, 4 ),
+            ( "CSS",  Tools.VisualStudio, 4),
+            ( "HTML",  Tools.VisualStudio, 4),
             ( "JS/TS", Tools.Prettier, 2 ),
-            ( "JS/TS",  Tools.VisualStudio2019, 4 ),
-            ( "JSON",  Tools.VisualStudio2019, 2 ),
-            ( "SQL",  Tools.VisualStudio2019, 4 ),
-            ( "XML",  Tools.VisualStudio2019, 2 )
+            ( "JS/TS",  Tools.VisualStudio, 4 ),
+            ( "JSON",  Tools.VisualStudio, 2 ),
+            ( "SQL",  Tools.VisualStudio, 4 ),
+            ( "XML",  Tools.VisualStudio, 2 )
         }.OrderBy(i => i.language);
 
         private static class Tools
         {
-            public const string VisualStudio2019 = "Visual Studio 2019";
+            public const string VisualStudio = "Visual Studio";
             public const string Prettier = "Prettier";
         }
     }
