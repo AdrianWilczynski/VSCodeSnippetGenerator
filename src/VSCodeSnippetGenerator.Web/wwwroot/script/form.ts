e150bdcaf215fdcfccbf38725b6536942c86f343
AdrianWilczynski <adrian.k.wilczynski@gmail.com>
2019-03-15 01:43:10 +0100

    Add button for copying snippets to clipboard

--- src/VSCodeSnippetGenerator.Web/wwwroot/script/form.ts
+++ src/VSCodeSnippetGenerator.Web/wwwroot/script/form.ts
@@ -8,4 +8,11 @@ let tabLengthInput = document.getElementById('TabLength') as HTMLInputElement;
 tabLengthInput.addEventListener('input', event => {
     let convertToTabsCheckbox = document.getElementById('ConvertToTabs') as HTMLInputElement;
     convertToTabsCheckbox.checked = !!tabLengthInput.value;
-})
+});
+
+let copyToClipboardButton = document.getElementById('CopyToClipboard');
+copyToClipboardButton.addEventListener('click', event => {
+    let snippetArea = document.getElementById('Snippet') as HTMLTextAreaElement;
+    snippetArea.select();
+    document.execCommand('copy');
+});
