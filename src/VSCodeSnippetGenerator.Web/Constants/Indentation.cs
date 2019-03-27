0197ac2155ecd2e51612ef057e823d2ef0f53255
AdrianWilczynski <adrian.k.wilczynski@gmail.com>
2019-03-27 20:09:37 +0100

    Get rid of labels from datalist since they seem to be broken in Chrome now

--- src/VSCodeSnippetGenerator.Web/Constants/Indentation.cs
+++ src/VSCodeSnippetGenerator.Web/Constants/Indentation.cs
@@ -5,22 +5,6 @@ namespace VSCodeSnippetGenerator.Web.Constants
 {
     public static class Indentation
     {
-        public static IEnumerable<(string language, string tool, int tabLength)> PerLanguage => new List<(string language, string, int)>
-        {
-            ( "C#", Tools.VisualStudio, 4 ),
-            ( "CSS",  Tools.VisualStudio, 4),
-            ( "HTML",  Tools.VisualStudio, 4),
-            ( "JS/TS", Tools.Prettier, 2 ),
-            ( "JS/TS",  Tools.VisualStudio, 4 ),
-            ( "JSON",  Tools.VisualStudio, 2 ),
-            ( "SQL",  Tools.VisualStudio, 4 ),
-            ( "XML",  Tools.VisualStudio, 2 )
-        }.OrderBy(i => i.language);
-
-        private static class Tools
-        {
-            public const string VisualStudio = "Visual Studio";
-            public const string Prettier = "Prettier";
-        }
+        public static IEnumerable<int> SpacesPerTab => new List<int> { 2, 4 };
     }
 }
