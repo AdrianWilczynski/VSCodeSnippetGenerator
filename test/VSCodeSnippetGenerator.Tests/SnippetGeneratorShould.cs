f18f17a52e653201a6b78e811b5567bc51559512
AdrianWilczynski <adrian.k.wilczynski@gmail.com>
2019-10-13 15:56:54 +0200

    Add tests for empty/multiline snippets and spaces to tabs conversion. Simplify.

--- test/VSCodeSnippetGenerator.Tests/SnippetGeneratorShould.cs
+++ test/VSCodeSnippetGenerator.Tests/SnippetGeneratorShould.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Text.RegularExpressions;
 using VSCodeSnippetGenerator.Web.Models;
 using VSCodeSnippetGenerator.Web.Services;
@@ -25,13 +26,80 @@ namespace VSCodeSnippetGenerator.Tests
 
             var lines = Regex.Split(snippet, @"\r?\n");
 
-            Assert.Matches("^\"Property\": {$", lines[0]);
-            Assert.Matches("^\\s+\"prefix\": \"prop\",$", lines[1]);
-            Assert.Matches("^\\s+\"body\": \\[$", lines[2]);
-            Assert.Matches("^\\s+\"public \\${1:int} \\${2:MyProperty} { get; set; }\\$0\"$", lines[3]);
-            Assert.Matches("^\\s+],$", lines[4]);
-            Assert.Matches("^\\s+\"description\": \"An automatically implemented property.\"$", lines[5]);
-            Assert.Matches("^}$", lines[6]);
+            Assert.Equal("\"Property\": {", lines[0]);
+            Assert.Equal("  \"prefix\": \"prop\",", lines[1]);
+            Assert.Equal("  \"body\": [", lines[2]);
+            Assert.Equal("    \"public ${1:int} ${2:MyProperty} { get; set; }$0\"", lines[3]);
+            Assert.Equal("  ],", lines[4]);
+            Assert.Equal("  \"description\": \"An automatically implemented property.\"", lines[5]);
+            Assert.Equal("}", lines[6]);
+        }
+
+        [Fact]
+        public void GenerateEmptySnippet()
+        {
+            var snippet = new SnippetGenerator().GetSnippet(SnippetInput.Empty);
+
+            var lines = Regex.Split(snippet, @"\r?\n");
+
+            Assert.Equal("\"\": {", lines[0]);
+            Assert.Equal("  \"prefix\": \"\",", lines[1]);
+            Assert.Equal("  \"body\": [", lines[2]);
+            Assert.Equal("    \"\"", lines[3]);
+            Assert.Equal("  ]", lines[4]);
+            Assert.Equal("}", lines[5]);
+        }
+
+        [Fact]
+        public void GenerateMultilineSnippet()
+        {
+            var input = new SnippetInput
+            {
+                Name = "Property",
+                Prefix = "prop",
+                Body = "public ${1:int} ${2:MyProperty} { get; set; }$0" + Environment.NewLine + "// Second Line"
+            };
+
+            var generator = new SnippetGenerator();
+
+            var snippet = generator.GetSnippet(input);
+
+            var lines = Regex.Split(snippet, @"\r?\n");
+
+            Assert.Equal("\"Property\": {", lines[0]);
+            Assert.Equal("  \"prefix\": \"prop\",", lines[1]);
+            Assert.Equal("  \"body\": [", lines[2]);
+            Assert.Equal("    \"public ${1:int} ${2:MyProperty} { get; set; }$0\",", lines[3]);
+            Assert.Equal("    \"// Second Line\"", lines[4]);
+            Assert.Equal("  ]", lines[5]);
+            Assert.Equal("}", lines[6]);
+        }
+
+
+        [Fact]
+        public void ConvertSpacesToTabs()
+        {
+            var input = new SnippetInput
+            {
+                Name = "Property",
+                Prefix = "prop",
+                Body = "    public ${1:int} ${2:MyProperty} { get; set; }$0",
+                ConvertToTabs = true,
+                TabLength = 4
+            };
+
+            var generator = new SnippetGenerator();
+
+            var snippet = generator.GetSnippet(input);
+
+            var lines = Regex.Split(snippet, @"\r?\n");
+
+            Assert.Equal("\"Property\": {", lines[0]);
+            Assert.Equal("  \"prefix\": \"prop\",", lines[1]);
+            Assert.Equal("  \"body\": [", lines[2]);
+            Assert.Equal("    \"\\tpublic ${1:int} ${2:MyProperty} { get; set; }$0\"", lines[3]);
+            Assert.Equal("  ]", lines[4]);
+            Assert.Equal("}", lines[5]);
         }
     }
 }
